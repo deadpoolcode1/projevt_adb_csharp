35e5b4d6e8a03578e0ad4545a500c2f420443ea2
ilanganor <ilan@kamacode.com>
2022-12-05 17:11:10 +0200

    sample for using standard ADB commands , working with commodore service

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using SharpAdbClient;
+using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            AdbServer server = new AdbServer();
+            AdbClient client = new AdbClient();
+            var result = server.StartServer(@"C:\adb\adb.exe", restartServerIfNewer: true);
+            var devices = client.GetDevices();
+            var device = client.GetDevices().First();
+            var receiver = new ConsoleOutputReceiver();
+            client.ExecuteRemoteCommand("service call ate.service 1 s16 android_command s16 ProductId", device, receiver);
+            Console.WriteLine("The device responded:");
+            Console.WriteLine(receiver.ToString());
+        }
+    }
+}
